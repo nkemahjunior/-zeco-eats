7bce6aa527b51b0e599f09227f7491895cb3dec8
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-01-03 07:32:05 +0100

    feat: categories modal ui

--- apps/zeco-eats-manager/src/features/menu/components/categories/Categories.tsx
+++ apps/zeco-eats-manager/src/features/menu/components/categories/Categories.tsx
@@ -1,8 +1,13 @@
 'use client'
+import Button from '@/shared/components/button/Button'
 import ButtonWithIcon from '@/shared/components/button/ButtonWithIcon'
+import ImageContainer from '@/shared/components/image/ImageContainer'
 import TextInput from '@/shared/components/inputs/TextInput'
 import TextInputWithIcon from '@/shared/components/inputs/TextInputWithIcon'
+import Line from '@/shared/components/Line'
+import CloseBtn from '@/shared/components/modal/CloseBtn'
 import Heading from '@/shared/components/text/Heading'
+import Heading2 from '@/shared/components/text/Heading2'
 import {
   ModalContext,
   modalContextTypes,
@@ -10,11 +15,23 @@ import {
 import { ChangeEvent, useContext, useState } from 'react'
 import { BiPlus, BiSave } from 'react-icons/bi'
 
+type item = {
+  name: string
+  price: string
+  image: string
+}
+
 const fakeCategories = Array.from({ length: 5 }, (_, i) => {
   return {
     name: `Fries${i}`,
     Menu: `My Place Menu`,
-    Items: `${i}Items`,
+    Items: [
+      { name: `Burger`, price: '£12.02', image: 'bla bla' },
+      { name: `Fries Rice`, price: '£12.02', image: 'bla bla' },
+      { name: `Porridge yam`, price: '£12.02', image: 'bla bla' },
+      { name: `Achu`, price: '£12.02', image: 'bla bla' },
+      { name: `Burger Mino`, price: '£12.02', image: 'bla bla' },
+    ], // all items for a category should come here
   }
 })
 
@@ -36,16 +53,46 @@ export default function Categories() {
     setSearchCategories(arr)
   }
 
-  const openCategory = () => {
+  const openCategory = (categoryName: string, items: item[]) => {
     openModal(
-      <div className="h-full border-2 border-solid border-red-700 px-4 py-4">
-        {' '}
-        content
+      <div className="h-full space-y-4 px-4 py-4">
+        <div className="sticky top-0 z-[1] space-y-2 bg-white py-2">
+          <div className="flex w-full items-center justify-end">
+            <CloseBtn />
+          </div>
+
+          <div className="space-y-4">
+            <Heading2 text={categoryName} />
+          </div>
+          <Line />
+        </div>
+
+        <div className="space-y-6">
+          {items.map((el) => (
+            <div key={el.name} className="flex items-center justify-between">
+              <div className="flex gap-x-4">
+                <ImageContainer
+                  src="/devImages/food1.webp"
+                  imageAlt=""
+                  height="h-20"
+                  width="w-20"
+                  roundedCorners="rounded-md"
+                />
+
+                <div className="flex flex-col gap-y-4">
+                  <span>{el.name}</span>
+                  <span>{el.price}</span>
+                </div>
+              </div>
+              <Button>Remove</Button>
+            </div>
+          ))}
+        </div>
       </div>,
       {
         ...modalProps,
-        height: ' h-[60%]',
-        width: 'w-[50%]',
+        height: ' min-h-[60%] max-h-[90%] ',
+        width: 'w-[30%]',
       }
     )
   }
@@ -101,7 +148,7 @@ export default function Categories() {
               <tr
                 key={i}
                 className="border-backgroundBorder hover:bg-background cursor-pointer transition-colors duration-300"
-                onClick={() => openCategory()}
+                onClick={() => openCategory(el.name, el.Items)}
               >
                 <td className="flex items-center space-x-4 px-4 py-4 text-start">
                   {el.name}
@@ -109,7 +156,9 @@ export default function Categories() {
 
                 <td className="px-4 py-4 text-start">{el.Menu}</td>
 
-                <td className="space-x-2 px-4 py-4 text-start">{el.Items}</td>
+                <td className="space-x-2 px-4 py-4 text-start">
+                  {el.Items.length}
+                </td>
               </tr>
             ))}
           </tbody>
