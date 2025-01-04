cddf017d761c04927e4a7aea2cf44f8839b76de8
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-01-04 13:48:01 +0100

    feat: edit customisations page 1

--- apps/zeco-eats-manager/src/features/menu/components/customisations/Customisation.tsx
+++ apps/zeco-eats-manager/src/features/menu/components/customisations/Customisation.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import Line from '@/shared/components/Line'
+import Heading from '@/shared/components/text/Heading'
 import {
   ModalUrlContext,
   modalUrlContextTypes,
@@ -13,12 +15,48 @@ export default function Customisation() {
   const { setModalUrlContent, modalUrlProps, setModalUrlProps } = useContext(
     ModalUrlContext
   ) as modalUrlContextTypes
-
   const updateParam = useUpdateUrlParams()
 
   const openCustomisation = () => {
     setModalUrlContent(
-      <div className="h-full w-full border-2 border-solid border-red-700"></div>
+      <div className="h-full px-6 py-8">
+        <div className="h-full w-full space-y-6">
+          <Heading text="Customisations" />
+          <div className="space-y-6">
+            <div>
+              <p className="font-medium">Customer Instructions</p>
+              <p>Chose Bread type</p>
+            </div>
+
+            <Line />
+
+            <div className="border-backgroundBorder flex cursor-pointer justify-between space-y-2 border-b border-solid py-6">
+              <div>
+                <p className="font-medium">Edit options</p>
+                <p>
+                  Manage which options are available to customers in this set
+                </p>
+              </div>
+              <MdKeyboardArrowRight size={20} />
+            </div>
+
+            <div className="border-backgroundBorder flex cursor-pointer justify-between space-y-2 border-b border-solid py-6">
+              <div>
+                <p className="font-medium">Add to items</p>
+              </div>
+              <MdKeyboardArrowRight size={20} />
+            </div>
+
+            <div className="flex cursor-pointer justify-between space-y-2 py-6">
+              <div>
+                <p className="font-medium">Customisation rules</p>
+                <p>Set limits for the options in this customisation</p>
+              </div>
+              <MdKeyboardArrowRight size={20} />
+            </div>
+          </div>
+        </div>
+      </div>
     )
     setModalUrlProps({
       ...modalUrlProps,
@@ -36,7 +74,7 @@ export default function Customisation() {
       onClick={openCustomisation}
     >
       <div className="flex flex-col justify-center space-y-2">
-        <span className="font-medium">Bread</span>
+        <span className="font-medium">Chose Bread type</span>
         <span>0 options</span>
       </div>
       <MdKeyboardArrowRight size={20} />
