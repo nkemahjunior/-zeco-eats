0436a9c6899f409e777ca049a2acb9c8d0003c8d
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-01-15 21:07:09 +0100

    feat: marketing nav bar

--- apps/zeco-eats-manager/src/features/marketing/MarketingNav.tsx
+++ apps/zeco-eats-manager/src/features/marketing/MarketingNav.tsx
@@ -1,33 +1,45 @@
 'use client'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { motion } from 'motion/react'
 
 export default function MarketingNav() {
   const pathname = usePathname()
-
   return (
     <div className="flex items-center gap-x-8 font-medium">
       <div className="flex flex-col justify-center gap-y-2">
-        <Link
-          href="/marketing/create-campaign"
-          className={` ${pathname == '/marketing/create-campaign' ? 'border-secondary' : 'border-transparent'} `}
-        >
-          Create campaign
-        </Link>
-        <div className="h-4 w-full bg-black"></div>
+        <Link href="/marketing/create-campaign">Create campaign</Link>
+        {pathname == '/marketing/create-campaign' ? (
+          <motion.div
+            layoutId="underlineLink"
+            className="h-1 w-full bg-black"
+          ></motion.div>
+        ) : (
+          <div className="h-1 w-full"></div>
+        )}
       </div>
-      <Link
-        href="/marketing/performance"
-        className={` ${pathname == '/marketing/performance' ? 'border-secondary' : 'border-transparent'} border-b-4 border-solid pb-2`}
-      >
-        Performance
-      </Link>
-      <Link
-        href="/marketing/campaigns"
-        className={` ${pathname == '/marketing/campaigns' ? 'border-secondary' : 'border-transparent'} border-b-4 border-solid pb-2`}
-      >
-        Campaigns
-      </Link>
+      <div className="flex flex-col justify-center gap-y-2">
+        <Link href="/marketing/performance">Performance</Link>
+        {pathname == '/marketing/performance' ? (
+          <motion.div
+            layoutId="underlineLink"
+            className="h-1 w-full bg-black"
+          ></motion.div>
+        ) : (
+          <div className="h-1 w-full"></div>
+        )}
+      </div>
+      <div className="flex flex-col justify-center gap-y-2">
+        <Link href="/marketing/campaigns">Campaigns</Link>
+        {pathname == '/marketing/campaigns' ? (
+          <motion.div
+            layoutId="underlineLink"
+            className="h-1 w-full bg-black"
+          ></motion.div>
+        ) : (
+          <div className="h-1 w-full"></div>
+        )}
+      </div>
     </div>
   )
 }
