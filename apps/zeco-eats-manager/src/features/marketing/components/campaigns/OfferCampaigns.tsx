2a3f0f3ad7c4cc451e48a25dac6b38b9805ee89a
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-01-17 18:38:10 +0100

    feat: offer Campaigns table

--- apps/zeco-eats-manager/src/features/marketing/components/campaigns/OfferCampaigns.tsx
+++ apps/zeco-eats-manager/src/features/marketing/components/campaigns/OfferCampaigns.tsx
@@ -7,7 +7,7 @@ export default function OfferCampaigns() {
       <div>
         <div className="flex w-full justify-center">
           <span className="border-backgroundBorder inline-block w-full rounded-tl-md border border-y border-l border-r border-solid p-4 font-medium">
-            Offer type
+            Ad type
           </span>
           <span className="border-backgroundBorder inline-block w-full border border-y border-l border-solid p-4 font-medium">
             Status
@@ -16,21 +16,25 @@ export default function OfferCampaigns() {
             Audience
           </span>
           <span className="border-backgroundBorder inline-block w-full border border-y border-l border-solid p-4 font-medium">
-            sales
+            TargetBid
           </span>
-          <span className="border-backgroundBorder inline-block w-full rounded-tr-md border border-y border-l border-solid p-4 font-medium">
+          <span className="border-backgroundBorder inline-block w-full border border-y border-l border-solid p-4 font-medium">
+            Daily budget
+          </span>
+
+          <span className="border-backgroundBorder inline-block w-full rounded-tr-md border border-y border-l border-r border-solid p-4 font-medium">
             Actions
           </span>
         </div>
 
         <div className="flex items-center rounded-bl-md rounded-br-md border-x border-b border-solid">
-          <span className="w-full p-4">Buy 1, Get 1 free</span>
+          <span className="w-full p-4">Ad campaign</span>
           <span className="flex w-full items-center gap-x-1 p-4">
             <CampaignStatusDot status="running" /> Active
           </span>
           <span className="inline-block w-full p-4">All customers</span>
           <span className="inline-block w-full p-4">£5.00</span>
-
+          <span className="inline-block w-full p-4">£5.25</span>
           <span className="inline-block w-full p-4">
             <MarketingCampaignAction />
           </span>
