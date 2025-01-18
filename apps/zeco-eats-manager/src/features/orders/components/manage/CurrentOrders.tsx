f6cf039ddba89c9a5b791a894ec5673d32b0f17a
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-01-18 20:09:01 +0100

    fix: manage orders page responsiveness

--- apps/zeco-eats-manager/src/features/orders/components/manage/CurrentOrders.tsx
+++ apps/zeco-eats-manager/src/features/orders/components/manage/CurrentOrders.tsx
@@ -35,9 +35,9 @@ export default function CurrentOrders() {
       </RestaurantStatusModalStackProvider>,
       {
         ...modalProps,
-        height: 'h-[55%]',
-        width: 'w-[25%]',
-        className: ' rounded-lg overflow-hidden',
+        height: 'h-full md:h-[45%] xl:h-[60%] 2xl:h-[55%]',
+        width: 'w-full md:w-[50%] xl:w-[35%] 2xl:w-[25%]',
+        className: ' md:rounded-lg overflow-hidden',
       }
     )
   }
@@ -45,11 +45,14 @@ export default function CurrentOrders() {
   return (
     <div>
       <div className="space-y-6">
-        <div className="flex items-center justify-between">
+        <div className="items-center justify-between space-y-4 md:flex md:space-y-0">
           <Heading text="Orders" />
 
-          <div className="flex gap-x-8">
-            <TextInputWithIcon id="searchOrderNum" width="w-[20rem]" />
+          <div className="flex gap-x-4 md:gap-x-8">
+            <TextInputWithIcon
+              id="searchOrderNum"
+              width="w-full md:w-[20rem]"
+            />
             <button
               className="bg-background hover:bg-backgroundShade2 flex h-[2.5rem] items-center justify-evenly gap-x-2 rounded-xl px-2"
               onClick={changeRestaurantStatus}
@@ -70,7 +73,7 @@ export default function CurrentOrders() {
           </div>
         </div>
 
-        <div className="grid grid-cols-2 gap-x-8">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-6 md:grid-cols-2 md:gap-y-0">
           <div className="space-y-4">
             <div className="space-x-2">
               <span className="text-base font-medium">Preparing</span>
