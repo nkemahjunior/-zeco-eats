90540bc4b4f2caf04d99beaa12637e9f647a4373
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-01-23 12:53:41 +0100

    fix: signup form desc text size

--- apps/zeco-eats-manager/src/features/auth/components/Signup/SignUpFormDesc.tsx
+++ apps/zeco-eats-manager/src/features/auth/components/Signup/SignUpFormDesc.tsx
@@ -1,14 +1,11 @@
-import Heading from '@/shared/components/text/Heading'
-
 export default function SignUpFormDesc() {
   return (
-    <div className="flex items-center bg-black/60">
+    <div className="flex items-center bg-black/60 px-32">
       <div className="w-full space-y-3 text-center">
-        <Heading
-          text="Join Zeco Eats: Expand Your Restaurant's Reach"
-          textColor="text-white"
-        />
-        <p className="text-white">
+        <h1 className="text-3xl font-bold text-white">
+          Join Zeco Eats: Expand Your Restaurant&apos;s Reach
+        </h1>
+        <p className="text-lg text-white">
           Sign up today to showcase your menu, connect with new customers, and
           boost your sales. Partner with us to grow your business effortlessly!
         </p>
