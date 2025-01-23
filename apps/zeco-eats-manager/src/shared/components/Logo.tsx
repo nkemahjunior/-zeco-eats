9650ddc98e3580f83c6fe84e94a5eb6d3fcf0e8c
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-01-23 12:52:31 +0100

    fix: logo accepts text color as props

--- apps/zeco-eats-manager/src/shared/components/Logo.tsx
+++ apps/zeco-eats-manager/src/shared/components/Logo.tsx
@@ -1,8 +1,16 @@
-export default function Logo() {
+interface fnProps {
+  color1?: string
+  color2?: string
+}
+
+export default function Logo({
+  color1 = 'text-secondary',
+  color2 = 'text-primary',
+}: fnProps) {
   return (
     <div className="font-extrabold">
-      <div className={`text-secondary text-nowrap text-xl`}>
-        Zeco <span className="text-primary">Eats</span> Manager
+      <div className={`${color1} text-nowrap text-xl`}>
+        Zeco <span className={`${color2}`}>Eats</span> Manager
       </div>
     </div>
   )
