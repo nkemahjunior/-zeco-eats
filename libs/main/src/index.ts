a8e3905c251f2e3ca09053c519a773bf5a02ee09
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-01-26 17:23:42 +0100

    refactor: change library name from utills to main

--- libs/main/src/index.ts
+++ libs/main/src/index.ts
@@ -1,6 +1,12 @@
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
 
+//SUPABASE
 export * from './lib/supabase/client'
+export * from './lib/supabase/types/database.types'
+
+//HOOKS
 export * from './lib/hooks/useDetectClickOutside'
 export * from './lib/hooks/useDebounce'
-export * from './lib/constants/cuisines'
+
+//TOAST
+export * from './lib/utils/toastMsgs'
