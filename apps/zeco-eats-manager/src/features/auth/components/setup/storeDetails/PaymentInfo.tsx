08674b6e512c358eeac8ac0fc7b220c5928082b7
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-01-29 17:25:06 +0100

    feat: payment info ui uncomplete

---
+++ apps/zeco-eats-manager/src/features/auth/components/setup/storeDetails/PaymentInfo.tsx
@@ -0,0 +1,16 @@
+import TextInput from '@/shared/components/inputs/TextInput'
+import Heading from '@/shared/components/text/Heading'
+
+export default function PaymentInfo() {
+  return (
+    <div>
+      <Heading text="Add you business banking info" />
+      <form action="">
+        {/* <div>
+                    <label htmlFor=""></label>
+                    <TextInput/>
+                </div> */}
+      </form>
+    </div>
+  )
+}
