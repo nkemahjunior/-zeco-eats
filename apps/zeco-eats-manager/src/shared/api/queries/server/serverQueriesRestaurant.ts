499a84511274c08ef8bf3bffc40e48499827acb0
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-02-07 23:26:51 +0100

    feat:  option to use client or server supabase

--- apps/zeco-eats-manager/src/shared/api/queries/server/serverQueriesRestaurant.ts
+++ apps/zeco-eats-manager/src/shared/api/queries/server/serverQueriesRestaurant.ts
@@ -1,24 +1,33 @@
 import { User } from '@supabase/supabase-js'
+import { createSupabaseClient } from '@zeco-eats-lib/utils-client'
 import { createSupabaseServer } from '@zeco-eats-lib/utils-server'
 
-export const getUser = async (): Promise<User> => {
-  const supabase = await createSupabaseServer()
+export const getUser = async (client?: boolean): Promise<User> => {
+  const supabase = client
+    ? createSupabaseClient()
+    : await createSupabaseServer()
   const {
     data: { user },
     error: authError,
   } = await supabase.auth.getUser()
   if (authError || !user) {
+    console.log(authError)
     throw authError
   }
 
   return user
 }
 
-export const getRestaurantId = async (): Promise<{
+export const getRestaurantId = async (
+  client?: boolean
+): Promise<{
   id: number
 }> => {
-  const user = await getUser()
-  const supabase = await createSupabaseServer()
+  const user = await getUser(client)
+  const supabase = client
+    ? createSupabaseClient()
+    : await createSupabaseServer()
+
   const { data: restaurantData, error: restaurantError } = await supabase
     .from('restaurant')
     .select('id')
@@ -26,6 +35,8 @@ export const getRestaurantId = async (): Promise<{
     .single()
 
   if (restaurantError || !restaurantData) {
+    console.log('restaurant error')
+    console.log(restaurantError)
     throw restaurantError
   }
 
