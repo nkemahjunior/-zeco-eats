9ec026d9d81c86e52e8a666a2a334eaf08820671
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-02-09 03:05:45 +0100

    feat: config next config images remote patterns

--- apps/zeco-eats-manager/next.config.ts
+++ apps/zeco-eats-manager/next.config.ts
@@ -42,6 +42,16 @@ const nextConfig = {
 
     return config
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ihydizyhbzwvaidfdunn.supabase.co',
+        pathname: '/storage/v1/object/public/zeco-eats/**',
+      },
+    ],
+  },
 }
 
 const plugins = [
