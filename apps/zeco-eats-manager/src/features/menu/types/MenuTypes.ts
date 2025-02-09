774749781ea0d156c85eb44c0143636b727c5a4a
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-02-09 03:13:31 +0100

    chore: add types

--- apps/zeco-eats-manager/src/features/menu/types/MenuTypes.ts
+++ apps/zeco-eats-manager/src/features/menu/types/MenuTypes.ts
@@ -1,6 +1,17 @@
 import { z } from 'zod'
 import { categorySchema, itemSchema, menuSchema } from './schemas/menuSchemas'
+import { Tables } from '@zeco-eats-lib/utils-client'
 
 export type Menu = z.infer<typeof menuSchema>
 export type Category = z.infer<typeof categorySchema>
 export type MenuItem = z.infer<typeof itemSchema>
+// export type MenuCategorieItem = Tables<'restaurant_menus_categories_items'> & {
+//   restaurant_menus: Tables<'restaurant_menus'>
+//   restaurant_items: Tables<'restaurant_items'>
+//   restaurant_categories: Tables<'restaurant_categories'>
+// }
+
+export interface MenuCategorieItem {
+  category: Tables<'restaurant_categories'>
+  items: Tables<'restaurant_items'>[]
+}
