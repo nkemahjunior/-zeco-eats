3821c2f1cd969e32ad72d691990de46c1a110cb0
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-02-09 03:10:50 +0100

    feat: edit categories

--- apps/zeco-eats-manager/src/features/menu/components/overview/ItemCategories.tsx
+++ apps/zeco-eats-manager/src/features/menu/components/overview/ItemCategories.tsx
@@ -1,18 +1,23 @@
 import { BiX } from 'react-icons/bi'
+import { MenuCategorieItem } from '../../types/MenuTypes'
 
 export default function ItemCategories({
   category,
   deleteCategory,
+  disable,
 }: {
-  category: string
-  deleteCategory: (arg: string) => void
+  category: MenuCategorieItem
+  deleteCategory: (arg: MenuCategorieItem) => void
+  disable: boolean
 }) {
   return (
-    <div className="bg-background w-fit rounded-lg px-3 py-2 font-medium">
+    <div
+      className={`${disable ? 'bg-background/50 pointer-events-none cursor-not-allowed' : 'bg-background pointer-events-auto cursor-auto'} w-fit rounded-lg px-3 py-2 font-medium`}
+    >
       <div className="flex items-center space-x-2">
-        <span>{category}</span>
+        <span>{category.category.name}</span>
         <button
-          className="hover:bg-backgroundShade2 transition-colors duration-300"
+          className={`hover:bg-backgroundShade2 transition-colors duration-300`}
           onClick={() => deleteCategory(category)}
         >
           <BiX size={20} />
