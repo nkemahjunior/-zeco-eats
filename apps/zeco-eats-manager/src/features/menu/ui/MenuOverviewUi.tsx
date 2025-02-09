9da206fd2a04d0b1660d20bfe086e3fa9a8fa6fd
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-02-09 03:16:30 +0100

    feat: add suspense boundaries

--- apps/zeco-eats-manager/src/features/menu/ui/MenuOverviewUi.tsx
+++ apps/zeco-eats-manager/src/features/menu/ui/MenuOverviewUi.tsx
@@ -3,17 +3,33 @@ import MenuHours from '../components/overview/MenuHours'
 import MenuTitle from '../components/overview/MenuTitle'
 import MenuCategoryAndItems from '../components/overview/MenuCategoryAndItems'
 import { getQueryClient } from '@/shared/api/tanstackQuery/get-query-client'
-import { restaurantMenusOptions } from '../api/queries/options/menuOptions'
+import {
+  restaurantCategoriesOptions,
+  restaurantMenuCategoriesItemsOptions,
+  restaurantMenusOptions,
+} from '../api/queries/options/menuOptions'
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
+import { Suspense } from 'react'
+import MenuTitleSkeleton from '../components/Skeletons/MenuTitleSkeleton'
+import MenuHoursSkeleton from '../components/Skeletons/MenuHoursSkeleton'
 
-export default function MenuOverviewUi() {
+export default function MenuOverviewUi({ menuId }: { menuId: string }) {
   const queryClient = getQueryClient()
   queryClient.prefetchQuery(restaurantMenusOptions)
+  queryClient.prefetchQuery(
+    restaurantMenuCategoriesItemsOptions(Number(menuId))
+  )
+  queryClient.prefetchQuery(restaurantCategoriesOptions)
+
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <div className="space-y-8">
-        <MenuTitle />
-        <MenuHours />
+        <Suspense fallback={<MenuTitleSkeleton />}>
+          <MenuTitle />
+        </Suspense>
+        <Suspense fallback={<MenuHoursSkeleton />}>
+          <MenuHours />
+        </Suspense>
         <Line />
         <MenuCategoryAndItems />
       </div>
