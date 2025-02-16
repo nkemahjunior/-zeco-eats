44950ed2dc00a39cc70b51891c727a491fbfa0da
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-02-16 23:26:04 +0100

    feat: add gradient angle prop

--- libs/main/src/lib/components/Shimmer.tsx
+++ libs/main/src/lib/components/Shimmer.tsx
@@ -1,15 +1,20 @@
 'use client'
 import { motion } from 'motion/react'
 
-export const Shimmer = ({ className }: { className: string }) => {
+export const Shimmer = ({
+  className,
+  angle = '135deg', // 90deg
+}: {
+  className: string
+  angle?: string
+}) => {
   return (
     <motion.div
       className={`relative overflow-hidden ${className}`}
       animate={{ backgroundPosition: ['200%', '-200%'] }}
       transition={{ repeat: Infinity, duration: 1.5, ease: 'linear' }}
       style={{
-        background:
-          'linear-gradient(90deg, #e0e0e0 25%, #f5f5f5 50%, #e0e0e0 75%)',
+        background: `linear-gradient(${angle}, #e0e0e0 25%, #f5f5f5 50%, #e0e0e0 75%)`,
         backgroundSize: '200% 100%',
       }}
     />
