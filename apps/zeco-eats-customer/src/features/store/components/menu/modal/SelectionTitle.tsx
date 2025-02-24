7e99b71192bb48d81367650dc812430a4032511a
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-02-24 11:15:38 +0100

    feat: dont show customisation status

--- apps/zeco-eats-customer/src/features/store/components/menu/modal/SelectionTitle.tsx
+++ apps/zeco-eats-customer/src/features/store/components/menu/modal/SelectionTitle.tsx
@@ -1,5 +1,5 @@
+'use client'
 import CardTitle from '@/shared/components/text/CardTitle'
-import SelectionStatus from './SelectionStatus'
 
 export default function SelectionTitle({
   title,
@@ -9,14 +9,19 @@ export default function SelectionTitle({
   title: string
   status: string
   chooseAmt: number
+  min: number
+  parentCustomisationId: number
 }) {
   return (
     <div>
       <div className="flex items-center justify-between">
         <CardTitle text={`${title}`} className="font-semibold" />
-        <SelectionStatus status={`${status}`} />
+        {/* <SelectionStatus
+          status={status}
+          color={customisationQtyError ? 'bg-red-200' : 'bg-background'}
+        /> */}
       </div>
-      <p className="text-storeTextColorTint">
+      <p className="bgre text-storeTextColorTint">
         Choose up to {chooseAmt} option(s)
       </p>
     </div>
