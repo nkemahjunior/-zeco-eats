febe1898740dd6a4eeed9ebf10f6753934841b43
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-02-24 11:14:12 +0100

    feat: push to cart onclick

--- apps/zeco-eats-customer/src/features/store/components/menu/modal/ModalButtons.tsx
+++ apps/zeco-eats-customer/src/features/store/components/menu/modal/ModalButtons.tsx
@@ -11,12 +11,15 @@ export default function ModalButtons({
   itemId: number
 }) {
   const storeId = useStoreId()
-  const { numItemsSeleted, totalModalPrice } = useDishInfoModal()
+  const { numItemsSeleted, totalModalPrice, pushToCart } = useDishInfoModal()
   return (
     <div className="sticky bottom-0 -mx-4 bg-white px-2 py-4 lg:static lg:bottom-auto lg:-mx-0 lg:px-0 lg:py-0">
       {/* <div className="border-green-70 h-[0.5rem] w-full border-2 border-solid bg-[#03081f] blur-lg lg:hidden mb-8"></div> */}
       <div className="space-y-4 font-medium transition-colors duration-300">
-        <button className="flex h-[4rem] w-full items-center justify-center rounded-lg bg-secondary text-white hover:bg-secondaryTint">
+        <button
+          className="flex h-[4rem] w-full items-center justify-center rounded-lg bg-secondary text-white hover:bg-secondaryTint"
+          onClick={pushToCart}
+        >
           <span>
             Add {numItemsSeleted} to order &middot; XAF {totalModalPrice}
           </span>
