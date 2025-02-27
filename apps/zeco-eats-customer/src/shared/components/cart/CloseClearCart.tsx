19abbc62106a1f3491b5155122291576bf5c7ea5
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-02-27 17:36:35 +0100

    fix: typo

--- apps/zeco-eats-customer/src/shared/components/cart/CloseClearCart.tsx
+++ apps/zeco-eats-customer/src/shared/components/cart/CloseClearCart.tsx
@@ -8,7 +8,7 @@ export default function CloseClearCart({
 }: {
   setOpen: (arg: boolean) => void
 }) {
-  const closeCart = useCartStore((state) => state.resetCart)
+  const clearCart = useCartStore((state) => state.resetCart)
 
   return (
     <div className="flex w-full items-center justify-between pb-4">
@@ -16,7 +16,7 @@ export default function CloseClearCart({
 
       <button
         className="flex h-[2rem] items-center justify-center space-x-2 rounded-lg bg-background px-4 font-medium text-red-600 transition-colors duration-300 hover:bg-backgroundShade2"
-        onClick={closeCart}
+        onClick={clearCart}
       >
         <span>
           <BiTrash />
