6580f364042b8c326867fa895ede27e852fee3c2
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-02-27 17:35:00 +0100

    feat: delete item

--- apps/zeco-eats-customer/src/shared/components/cart/CartIncreaseDecreaseQty.tsx
+++ apps/zeco-eats-customer/src/shared/components/cart/CartIncreaseDecreaseQty.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useCartStore } from '@/stores/globalStore'
 import { BiMinus, BiPlus } from 'react-icons/bi'
+import { MdDelete } from 'react-icons/md'
 
 export default function CartIncreaseDecreaseQty({
   qty,
@@ -10,17 +11,15 @@ export default function CartIncreaseDecreaseQty({
   itemId: number
 }) {
   const changeItemQty = useCartStore((state) => state.changeItemQty)
+  const removeFromCart = useCartStore((state) => state.removeFromCart)
   return (
     <div className="flex items-center space-x-4 rounded-3xl bg-backgroundShade1 px-3 py-2 hover:bg-backgroundShade2">
       <button
-        className={`disabled:cursor-not-allowed disabled:bg-background disabled:text-stone-400`}
-        onClick={() => changeItemQty(itemId, 'dec')}
-        disabled={qty <= 1}
+        onClick={() =>
+          qty > 1 ? changeItemQty(itemId, 'dec') : removeFromCart(itemId)
+        }
       >
-        {' '}
-        <span>
-          <BiMinus />
-        </span>
+        <span>{qty > 1 ? <BiMinus /> : <MdDelete />}</span>
       </button>
       {/* {selectedQty < min ? null : <p>{selectedQty}</p>} */}
       <p>{qty}</p>
