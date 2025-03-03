82219a9815c269dc3fc961e4a6d88adf1f711555
Nkemah_Junior <nkemahjunior679205967@gmail.com>
2025-03-03 13:29:22 +0100

    feat: click to copy user's location

--- apps/zeco-eats-customer/src/features/store/components/storeHeader/StoreLocationItem.tsx
+++ apps/zeco-eats-customer/src/features/store/components/storeHeader/StoreLocationItem.tsx
@@ -1,7 +1,25 @@
+'use client'
+
+import { useState } from 'react'
 import { BiLocationPlus } from 'react-icons/bi'
 import { BsCopy } from 'react-icons/bs'
+import { toast } from 'sonner'
 
 export default function StoreLocationItem({ location }: { location: string }) {
+  const [isCopying, setIsCopying] = useState(false)
+
+  const handleCopy = async () => {
+    try {
+      setIsCopying(true)
+      await navigator.clipboard.writeText(location)
+      toast.success('Location copied!')
+    } catch (error) {
+      toast.error('Failed to copy')
+    } finally {
+      setIsCopying(false)
+    }
+  }
+
   return (
     <div className="flex">
       <div className="flex basis-[20%] items-center justify-center">
@@ -15,7 +33,11 @@ export default function StoreLocationItem({ location }: { location: string }) {
           <p className="text-storeTextColorTint">{location}</p>
         </div>
         <div className="flex basis-[20%] items-center justify-center">
-          <button>
+          <button
+            onClick={handleCopy}
+            disabled={isCopying}
+            className="transition hover:text-gray-600"
+          >
             <BsCopy size={20} />
           </button>
         </div>
@@ -23,18 +45,3 @@ export default function StoreLocationItem({ location }: { location: string }) {
     </div>
   )
 }
-//overflow-hidden text-ellipsis whitespace-nowrap  w-10
-
-/**
- *   const [isCopied, setIsCopied] = useState(false);
-
-  const handleCopy = async (textToCopy: string) => {
-    try {
-      await navigator.clipboard.writeText(textToCopy);
-      setIsCopied(true);
-      setTimeout(() => setIsCopied(false), 2000); // Reset after 2 seconds
-    } catch (error) {
-      console.error("Failed to copy text: ", error);
-    }
-  };
- */
